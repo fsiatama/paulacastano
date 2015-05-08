ed440fc3244207c28f85a0946722c4919820f6df
Fabian Siatama <fas0980@gmail.com>
2015-05-07 21:17:25 -0500

    adicion fuente iconos de galeria

---
+++ app/lang/es/contact.php
@@ -0,0 +1,15 @@
+<?php
+return [
+	'title' => 'E-mail o teléfono son bienvenidos',
+	'subtitle' => '',
+	'name' => 'Nombre',
+	'name_holder' => 'Ingrese Nombre Completo',
+	'email' => 'Email',
+	'email_holder' => 'Ingrese Correo Electrónico',
+	'subject' => 'Asunto',
+	'subject_opts' => [
+		'na' => 'Elige Uno:',
+	],
+	'message' => 'Mensaje',
+	'send' => 'Enviar Mensaje',
+];
